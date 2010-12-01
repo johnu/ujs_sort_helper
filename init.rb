ff7514f21d01cdd3aba43319b54911c9f6bb7bb3
Prem Sichanugrist <s@sikachu.com>
2010-12-01 17:10:48 +0700

    Use Rails 3's Generator format
    This will stop the problem when the plugin actually tries to copy the
    helper files upon initializing on read-only disk image.
    
    You'll have to run `rails generate ujs_sort_helper:install` to get those
    static assets copied to your public directory.

--- init.rb
+++ init.rb
@@ -2,11 +2,3 @@ require 'ujs_sort_helper'
 
 ActionView::Base.send(:include, UjsSortHelper)
 ActionController::Base.send(:include, UjsSortHelper)
-
-# install files
-['/public/javascripts', '/public/stylesheets', '/public/images'].each{|dir|
-  current_path = File.expand_path(File.dirname(__FILE__))
-  source = File.join(current_path,dir)
-  dest = current_path + '/../../..' + dir
-  FileUtils.cp_r(source+'/.', dest)
-} unless File.exists?(Rails.root + '/public/javascripts/ujs_sort_helper.js')
